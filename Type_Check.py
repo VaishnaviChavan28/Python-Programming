745dfd0e86c69f138da29bdb1077ba96497d3304
VaishnaviChavan28 <chavanvaishnavi331@gmail.com>
2026-06-17 11:43:02 +0530

    Data Type Check Program In Python

---
+++ Type_Check.py
@@ -0,0 +1,23 @@
+a=10
+print(a)
+print(type(a))
+
+
+b=20.30
+print(b)
+print(type(b))
+
+
+c='z'
+print(c)
+print(type(c))
+
+name="Ram"
+print(name)
+print(type(name))
+
+
+
+para='''       Hello I am Vaishnavi
+               Computer Student......       '''
+print(para)
