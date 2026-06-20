9a44681f39e241d57db46188224764420d20000b
VaishnaviChavan28 <chavanvaishnavi331@gmail.com>
2026-06-20 20:37:16 +0530

    Operators in Python

---
+++ Operators.py
@@ -0,0 +1,112 @@
+# Arithmetic Operator
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b) # a Raise to power b ...
+
+print()
+
+# Assignment Operator
+x=90
+x+=90
+print(x)
+
+print("---------------------------------")
+
+z=124
+print(z)
+z-=5
+print(z)
+z*=2
+print(z)
+z+=4
+print(z)
+z/=9
+print(z)
+z%=3
+print(z)
+
+print("---------------------------------")
+
+print()
+
+
+# Logical Operator
+a=10
+b=20
+print(a>b and b>a)
+print(a<b or a==b)
+print(not(a>b and a<b))
+
+print()
+
+
+
+# Bitwise Operator
+
+
+#bitwise and
+a=2
+b=3
+print(a&b)
+
+#bitwise or
+a=2
+b=3
+print(a|b)
+
+#bitwise x-or
+a=2
+b=3
+print(2^3)
+
+#complement not
+print(~15)
+
+
+print()
+
+
+# Relational Operator
+a=20
+b=15
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+print()
+
+
+
+# MemberShip Operator
+x=[10,20,30]
+print(20 in x)
+print(50 in x)
+
+print(10 not in x)
+print(40 not in x)
+
+
+print()
+
+
+# Identity Operator
+x=[1,2,3]
+z=[5,6,7]
+y=x
+print(x)
+print(y)
+print()
+print(x is y)
+print(x is z)
+
+print(x is not z)
+
+print()
