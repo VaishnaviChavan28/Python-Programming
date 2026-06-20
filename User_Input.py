79eb8dafda1b329fc6b72cd58ff3f0dc90632c16
VaishnaviChavan28 <chavanvaishnavi331@gmail.com>
2026-06-20 20:29:38 +0530

    Taking Input from user

---
+++ User_Input.py
@@ -0,0 +1,13 @@
+name=input("Enter Your Name : ")
+print("Your Name is : ",name)
+
+
+a=int(input("Enter 1st Number : "))
+b=int(input("Enter 2nd Number : "))
+print("Sum = ",a+b)
+print("Power = ",a**b)
+
+
+a=float(input("Enter 1st Number : "))
+b=float(input("Enter 2nd Number : "))
+print("Sum = ",a+b)
