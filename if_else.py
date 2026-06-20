f7e33aaeb15e80164a4131c12089be5b7fcf785c
VaishnaviChavan28 <chavanvaishnavi331@gmail.com>
2026-06-20 20:26:56 +0530

    Python if-else Programs

---
+++ if_else.py
@@ -0,0 +1,56 @@
+
+# 1 :- 
+num=int(input("Enter Your Number : "))
+if num%2==0 :
+    print("Even Number")
+else : 
+    print("Odd Number")
+
+print()
+
+
+
+# 2 :-
+age=int(input("Enter Your Age : "))
+if age>=18 :
+    print("Eligiable For Voting ")
+else :
+    print("Not Eligiable For Voting ")
+
+print()
+
+
+
+# 3 :- 
+number=int(input("Enter  a Number : "))
+if number%5==0 and number%7==0 :
+    print("Number is Divisble by 5 and 7")
+else :
+    print("Number is not Divisble by 5 and 7")
+
+print()
+
+
+
+
+# 4 :-
+num1=int(input("Enter 1st number : "))
+num2=int(input("Enter 2nd Number : "))
+if num1>num2 :
+    print("Num1 is greater")
+else :
+    print("Num2 is greater")
+
+print()
+
+
+
+
+# 5 :-
+char=input("Enter any Character : ")
+if char>='a' and char<='z' :
+    print("LowerCase Letter")
+else :
+    print("UpperCase Letter")
+
+print()
