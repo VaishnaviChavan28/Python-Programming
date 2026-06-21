8562fd2da3bc6c61c4bdb8dadbac7fed45f0f706
VaishnaviChavan28 <chavanvaishnavi331@gmail.com>
2026-06-21 20:54:50 +0530

    Type Casting in Python

---
+++ Type_Casting.py
@@ -0,0 +1,12 @@
+
+# float to int TypeCasting
+a=int(float(input("Enter a Decimal Number : ")))
+print(a)
+print(type(a))
+
+
+# Int To Float TypeCasting
+b=float(int(input("Enter a Integer Number : ")))
+print(b)
+print(type(b))
+
